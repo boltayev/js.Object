a7517636f80c9284e58b589ae55a4a873d918aaa
jobneewcont <lisodev382@invodua.com>
2023-01-16 15:21:57 +0500

    I created objekt

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+//----HOMEWORK-------
+
+let name2 =prompt('Ismingizni kiriting')
+let surname =prompt('Familiyangizni kiriting')
+let age =+prompt('Yoshingizni kiriting')
+
+let obj = {
+
+    name1: name2,
+    surname1: surname,
+    age1: age,
+}
+
+let revers = obj
+console.log(revers);
