fe483925ff201cd6da3db097cb77645b26f85007
jobneewcont <lisodev382@invodua.com>
2023-01-22 22:06:21 +0500

    I add homework

--- js/script.js
+++ js/script.js
@@ -1,16 +1,412 @@
 
-//----HOMEWORK-------
+// //----HOMEWORK-------
 
-let name2 =prompt('Ismingizni kiriting')
-let surname =prompt('Familiyangizni kiriting')
-let age =+prompt('Yoshingizni kiriting')
+// let name2 =prompt('Ismingizni kiriting')
+// let surname =prompt('Familiyangizni kiriting')
+// let age =+prompt('Yoshingizni kiriting')
 
-let obj = {
+// let obj = {
 
-    name1: name2,
-    surname1: surname,
-    age1: age,
+//     name1: name2,
+//     surname1: surname,
+//     age1: age,
+// }
+
+// let revers = obj
+// console.log(revers);
+
+
+// const text = document.querySelector('p')
+// // console.log(text.style);
+// text.style.fontSize  = '50px',
+// text.style.fontWeight  = '100px'
+// text.style.color = 'red'
+
+// const dd = document.querySelectorAll('p')
+// dd.forEach((text) => {
+//     if(text.textContent.includes('are')){
+//         text.classList.add('mmm')
+//     }
+//      else if(text.textContent.includes('am')){
+//         text.classList.add('rrr')
+//     }
+// })
+
+// const dev = document.querySelector('section')
+// console.log(dev.children);
+
+// (Array.from(dev.children)).forEach((num) =>{
+//     num.classList.add('children_classs')
+// })
+
+// let name2 =prompt('Ismingizni kiriting')
+// let surname =prompt('Familiyangizni kiriting')
+// let age =+prompt('Yoshingizni kiriting')
+
+// let obj = {
+
+//     name1: name2,
+//     surname1: surname,
+//     age1: age,
+// }
+
+// let revers = obj
+// console.log(revers);
+
+
+// let name1 =prompt("ism kiriting")
+// let age =+prompt('yosh kiriting')
+
+// let name2 =prompt('ismingizni kiriting')
+// let age2 =+prompt('yoshingizni  kiriting')
+
+
+// let user1 ={
+//     names: name1,
+//     ages1: age
+// }
+
+// let user2 ={
+//     names: name2,
+//     ages2: age2
+// }
+
+// if(user1.ages1 > user2.ages2){
+//     console.log(katta);
+// }
+// else{
+//     console.log(kichik);
+// }
+
+
+
+// let qq = user1
+// let qqq = user2
+
+
+// console.log(qq);
+// console.log(qqq);
+
+// if(user1 > user2){
+//     console.log(katta);
+// }
+// else{
+//     console.log(kichik);
+// }
+
+// let name1 =prompt("ism kiriting")
+// let age =+prompt('yosh kiriting')
+
+// let name2 =prompt('ismingizni kiriting')
+// let age2 =+prompt('yoshingizni  kiriting')
+
+
+// let user1 ={
+//     names: name1,
+//     ages1: age
+// }
+
+// let user2 ={
+//     names: name2,
+//     ages2: age2
+// }
+
+
+// if(user1.ages1 > user2.ages2){
+//     console.log('Birinchi kiritilgan shaxsning yoshi katta');
+// }
+// else{
+//     console.log('Ikkinchi kiritilgan shaxsni yoshi kichik');
+// }
+
+
+// let num =+prompt('1-sonni kiriting')
+// let num1 =+prompt('amal')
+// let num2 =+prompt('2-sonni kiriting')
+
+ 
+// if(num * num2 == c){
+//     console.log(c);
+// }
+
+// for(let i = 0; i < arr.length; i++){
+//     if(arr.length < 2){
+//         console.log('sswsw:' )
+//     }
+//     else{
+//         console.log('wdww');
+//     }
+// }
+
+
+// let arr = ['Azizbek', 'Bilol', 'Asror', 'Ikromjon','fr', 'de']
+
+// // let sum = []
+
+// arr.forEach((dew) =>{
+//     if(dew.length >= 5){
+//         console.log('katta')
+//     }
+//     else{
+//         console.log('kichik');
+//     }
+// })
+
+    // let arr = prompt('biror narsa kiriting')
+    // let s = arr.split(" ")
+    // s.forEach((num) =>{
+    //     console.log(num, num.length);
+    // })
+
+    // let arr = ('dcdcdd', 'dcdcd','d','ssss')
+    // let p = arr.split(" ")
+    // let oo = ' '
+    //     for (let i = 0; i < p.length; i++){
+    //         oo+= (p[i] + " " + p[i].length);
+    //         console.log(oo);
+
+    //     }
+
+    // let arr  = [1, 2, 3, 4, 8]
+    // let fk = arr.split(" ")
+    // let d = ' '
+
+    // for ( let i = 0; i < fk.length; i++){
+    //     console.log(fk);
+    // }
+
+
+
+
+    // let d = []
+    // arr.forEach((num) =>{
+    //     d = d * num
+    //     return d
+    // })
+    // console.log(
+    //     d
+    // );
+
+
+//     function addLength(str) {
+//   let dd = []
+//   let dew = str.split('')
+//   dew.forEach((num) =>{
+//     dd(num,num.length)
+//   })
+//   return dd
+// }
+//     addLength('dwws')
+
+
+// let day =prompt('kiriting')
+// switch(day){
+//     case 1:
+//     console.log();
+//     break
+
+//     case 1:
+//     console.log();
+//     break
+
+//     case 1:
+//     console.log();
+//     break
+
+//     case 1:
+//     console.log();
+//     break
+
+//     default:
+//     console.log();
+// }
+
+
+// function evenOrOdd(number) {
+//   if (number % 2==0)
+//   {
+//     return "Even"
+//   }
+//   else
+//   {
+//     return "Odd"
+//   }
+// }
+// console.log(evenOrOdd(3))
+
+
+// function grow(dew){
+//   let sum = 1  
+//   for(let i = 0; i < dew.length; i++){
+//     sum *= dew[i]
+//   }
+//   dew.forEach((new) = >{
+//     sum+=new * i
+//   })
+  
+//   return sum
+// }
+
+// lis = [1, 2, 3, 4]
+// console.log(grow(lis))
+
+
+// let arr = [1,2,3,4,]
+
+// let sum = 1
+// arr.forEach((ss)=>{
+//     sum *=ss
+//     // return sum
+// })
+//     return sum
+
+
+
+
+
+// function sayHello(basic){
+//     let best = 1
+//     for(let i = 0; i < basic.length; i++){
+//         best *= basic[i]
+//     }
+//     return best
+// }
+// list =[1,2,3,4,8]
+// console.log(sayHello(list));
+
+ 
+// function grow(dew){
+//   let sum = 1  
+//   for(let i = 0; i < dew.length; i++){
+//     sum *= dew[i]
+//   }
+//   return sum
+// }
+
+// lis = [1, 2, 3, 4]
+// console.log(grow(lis))
+
+
+
+// function web(str){
+
+//     let arr = ""
+
+//     for(let i = 0; i < str.length; i++){
+//         if(str[i] === 'a' || str[i] === 'o' || str[i] === 'i' || str[i] === 'e' || str[i] === 'u' ){
+//             // continue
+//         }
+//         arr += str[i]
+
+//     }
+//     return arr
+// }
+
+// ff = 'dfdddweeadfd'
+// console.log(web(ff));
+
+
+// function sayHello(sew){
+//     oo = 'fekolila'
+
+//     let ww = ""
+//     for(let i = 0; i < sew.length; i++){
+//         if(!oo.includes(sew[i]))
+//         ww+=sew[i]
+//     }
+//     return ww
+//  }
+
+//  console.log(sayHello('dfd'));
+// sayHello()
+// oo = 'fekolila'
+// console.log(sayHello(oo));
+
+// function shortcut (string) {
+//   let newStr = ""
+  
+//   for (let i = 0; i < string.length; i++) {
+//     if(!'aeiou'.includes(string[i])) {
+//       newStr += string[i]
+//     }
+//   }
+  
+//   return newStr
+// }
+
+// console.log(shortcut('wee'))
+
+
+let arr = [
+    {
+        name: 'ELBEKJON'
+    },
+
+    {
+        name: 'ORIF'
+    },
+
+    {
+        name: 'javohir'
+    },
+
+    {
+        name: 'wer'
+    },
+
+    {
+        name: 'SARVAR'
+    },
+
+    {
+        name: 'GULCHEHRA'
+    },
+
+    {
+        name: 'JOB'
+    },
+
+    {
+        name: 'MALIK'
+    }
+
+]
+let ddd = []
+for( let i = 0; i < arr.length; i++){
+    if(arr[i].name.length > 5){
+        ddd.push(arr[i].name)
+    }
 }
+console.log(ddd, 'Ismlari uzuligi 5 dan katta');
 
-let revers = obj
-console.log(revers);
+let ggg = []
+for( let i = 0; i < arr.length; i++){
+    if(arr[i].name.length < 5){
+        ggg.push(arr[i].name)
+    }
+}
+console.log(ggg, 'Ismlari uzuligi 5 dan kichik');
+
+
+let yyy = []
+for( let i = 0; i < arr.length; i++){
+    if(arr[i].name.length == 5){
+        yyy.push(arr[i].name)
+    }
+}
+console.log(yyy, 'Ismlari uzuligi 5 ga teng');
+
+// --------  2- usul  ------------
+
+
+for(let i = 0; i < arr.length; i++){
+    if(arr[i].name.length > 5){
+        console.log(arr[i].name, "ismini uzunligi 5 dan katta");
+    }
+    else if(arr[i].name.length = 5 ){
+        console.log(arr[i].name, "ismini uzunligi 5 ga teng"); 
+    }
+    else {
+        console.log(arr[i].name, '5 dan kichik');
+    }
+}
